d3d2d8d3d4521cf685cdcbb59619367025d12b2a
Sreenath <sreenathkumar66@gmail.com>
2022-09-28 09:06:30 +0600

    hide color select

--- src/components/projects/AddProjectsModal.js
+++ src/components/projects/AddProjectsModal.js
@@ -58,7 +58,7 @@ export default function AddProjectsModal({ control, open }) {
                         <label htmlFor="team-title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Poject Tiltle</label>
                         <input type="text" name="team-title" id="team-title" className="outline-none bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Lorem Ipsum is simply dummy text" required value={title} onChange={(e) => setTitle(e.target.value)} />
                      </div>
-                     <div>
+                     <div className="hidden">
                         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Select a color</label>
 
                         <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
